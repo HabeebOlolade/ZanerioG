f6a48faf474fe6414f1cd7f62d684b9390e50e98
habeeb <tunjiololade@gmail.com>
2023-07-02 20:02:43 +0200

    update

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
